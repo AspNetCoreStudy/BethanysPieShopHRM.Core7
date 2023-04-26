53d2c9c6c3b91b3f1111c8ce873ae0401328c347
Marlon <marlonfsolisest@gmail.com>
2023-04-26 06:16:00 -0400

    feat: displaying employees in cards. Loading indicator component in place.

---
+++ BethanysPieShopHRM.App/Components/EmployeeCard.cs
@@ -0,0 +1,12 @@
+﻿using BethanysPieShopHRM.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieShopHRM.App.Components
+{
+    public partial class EmployeeCard
+    {
+        [Parameter]
+        public Employee? Employee { get; set; }
+
+    }
+}
